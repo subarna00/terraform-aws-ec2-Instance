736ba3a70e51e2480d95f67a5daea2f065247335
subarna00 <subarnauprety@gmail.com>
2023-08-11 09:26:37 +0545

    aws security group

--- instance.tf
+++ instance.tf
@@ -4,6 +4,25 @@ resource "aws_key_pair" "aws_tf_key_pair" {
   public_key = file("${path.module}/id_rsa.pub")
 }
 
+# createing security group for instance
+resource "aws_security_group" "aws_tf_security_group" {
+  name = "tf_security_group"
+  description = "Allow tls inbound traffice"
+
+# ingress is incomming and egress is outgoing
+ dynamic "ingress" {
+   for_each = [22,80,443,3306,27017]
+   iterator = port
+   content {
+     description = "exposed accessible  port"
+     from_port = port.value
+     to_port = port.value
+     protocol = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+ }
+}
+
 
 # creating ec2 instance
 resource "aws_instance" "ec2_instance" {
